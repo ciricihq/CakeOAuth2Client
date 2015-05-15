3b077caa2ed1b0fe05a42a15fc38746240a6ccad
Genar Trias Ortiz <genar@acs.li>
2015-05-15 14:48:16 +0200

    Preparing testing environment

---
+++ tests/TestCase/Auth/OAuth2AuthenticateTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace OAuth2Client\Test\TestCase\Auth;
+
+use Cake\TestSuite\TestCase;
+
+class OAuth2AuthenticateTest extends TestCase
+{
+    public function testAuthenticate()
+    {
+    }
+}
